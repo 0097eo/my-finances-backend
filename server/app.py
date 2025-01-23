f47ec0a0f9f48e7035f16f46cf075342e30f6627
0097eo <emmanuelokello294@gmail.com>
2025-01-23 09:03:15 +0300

    add transaction resource with CRUD operations for user transactions

--- server/app.py
+++ server/app.py
@@ -76,11 +76,80 @@ class Profile(Resource):
         except Exception as e:
             return {"error": str(e)}, 500
     
+class TransactionResource(Resource):
+    @jwt_required
+    def get(self):
+        try:
+            user_id = get_jwt_identity()
+            transactions = Transaction.query.filter_by(user_id=user_id).all()
+            return jsonify([{
+                "id": t.id,
+                "user_id": t.user_id,
+                "budget_category_id": t.budget,
+                "created_at": t.created_at,
+                "amount": t.amount,
+                "description": t.description,
+                "type": t.type
+                }for t in transactions])
+        except Exception as e:
+            return {"error": str(e)}, 500
+    
+    @jwt_required
+    def post(self):
+        try:
+            user_id = get_jwt_identity()
+            data = request.get_json()
+            required_fields = ["budget_category_id", "amount", "type", "description"]
 
+            for field in required_fields:
+                if field not in data:
+                    return {"error": f"Missing {field} field"}, 400
+            
+            category = BudgetCategory.query.get(data["budget_category_id"])
+            if not category:
+                return {"error": "Category does not exist"}, 400
+        
+            new_transaction = Transaction(
+                user_id=user_id,
+                budget_category_id=data["budget_category_id"],
+                amount=data["amount"],
+                description=data["description"],
+                type=data["type"]
+            )
+
+            db.session.add(new_transaction)
+            db.session.commit()
+
+            return {"message": "Transaction created successfully"}, 201
+        
+        except Exception as e:
+            db.session.rollback()
+            return {"error": str(e)}, 500
+        
+
+    @jwt_required
+    def delete(self, transaction_id):
+        try:
+            user_id = get_jwt_identity()
+            transaction = Transaction.query.get(transaction_id)
+            if not transaction:
+                return {"error": "Transaction does not exist"}, 400
+            if transaction.user_id != user_id:
+                return {"error": "Unauthorized"}, 401
+
+            db.session.delete(transaction)
+            db.session.commit()
+
+            return {"message": "Transaction deleted successfully"}, 200
+        
+        except Exception as e:
+            db.session.rollback()
+            return {"error": str(e)}, 500
 
 api.add_resource(Register, '/register')
 api.add_resource(Login, '/login')
 api.add_resource(Profile, '/profile')
+api.add_resource(TransactionResource, '/transactions', '/transactions/<int:transaction_id>')
 
 
 
